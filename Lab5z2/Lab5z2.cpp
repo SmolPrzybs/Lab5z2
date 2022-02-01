7106efb9eaed86e9c6e3681eb25572638e4f3ec0
SmolPrzybs <maciek970@gmail.com>
2022-02-01 22:55:08 +0100

    Dodaj pliki projektów.

---
+++ Lab5z2/Lab5z2.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+
+int NWD(int a, int b)
+{
+	if (b != 0)
+		return NWD(b, a % b);
+
+	return a;
+}
+
+int main()
+{
+	int a, b;
+	cout << "Podaj pierwsza liczbe: ";
+	cin >> a;
+	cout << "Podaj druga liczbe: ";
+	cin >> b;
+
+	cout << "NWD(" << a << "," << b << ") = " << NWD(a, b) << endl;
+
+	return 0;
+}
